482dc9b3239cbe35145b9521c2a46e229c4293ee
Mihai405 <mihaijurj2000@yahoo.com>
2022-01-04 16:57:34 +0200

    Add Swaggers

--- application/friends/views.py
+++ application/friends/views.py
@@ -1,4 +1,4 @@
-from flask_restx import Resource
+from flask_restx import Resource, fields
 from flask import request, session, abort
 from marshmallow import ValidationError
 from sqlalchemy.exc import IntegrityError
@@ -6,11 +6,32 @@ from sqlalchemy import and_
 from .models import Friends , FriendsSchema, UpdateFriendSchema
 from application.users.models import Users
 from application import db, cache
+from . import api
 
 friend_schema = FriendsSchema()
 friends_schema = FriendsSchema(many=True)
 
+user_fields = api.model('User', {
+    "id" : fields.Integer()
+})
+
+friend_fields = api.model('Friend',{
+    "first_name" : fields.String(required=True),
+    "last_name" : fields.String(required=True),
+    "number" : fields.String(required=True),
+    "user" : fields.Nested(user_fields)
+})
+
+friend_create_fields = api.model('FriendCreate',{
+    "first_name" : fields.String(required=True),
+    "last_name" : fields.String(required=True),
+    "number" : fields.String(required=True),
+})
+
 class FriendsList(Resource):
+
+    @api.doc(responses={401: 'Unauthorized'})
+    @api.marshal_with(friend_fields, as_list=True)
     @cache.cached(timeout=500)
     def get(self):
         try:
@@ -23,6 +44,8 @@ class FriendsList(Resource):
         friends=Friends.query.filter_by(user_id=session["user_id"]).all()
         return friends_schema.dump(friends)
 
+    @api.doc(responses={200: ('Success', friend_fields), 401: 'Unauthorized', 400:'Bad Request'},
+             body=friend_create_fields)
     def post(self):
         try:
             session["user_id"]
@@ -56,9 +79,18 @@ class FriendsList(Resource):
         cache.clear()
         return friend_schema.dump(new_friend)
 
+
 update_friend_schema = UpdateFriendSchema()
 
+friend_update_fields = api.model('FriendUpdate',{
+    "first_name" : fields.String(),
+    "last_name" : fields.String(),
+    "number" : fields.String(),
+})
+
 class FriendUpdate(Resource):
+
+    @api.doc(responses={200: ('Success', friend_fields), 401: 'Unauthorized', 400:'Bad Request'})
     @cache.cached(timeout=500)
     def get(self, friend_id):
         try:
@@ -78,6 +110,8 @@ class FriendUpdate(Resource):
             )
         return friend_schema.dump(friend)
 
+    @api.doc(responses={200: ('Success', friend_fields), 401: 'Unauthorized', 400:'Bad Request'},
+             body=friend_update_fields)
     def put(self, friend_id):
         try:
             session["user_id"]
@@ -123,6 +157,7 @@ class FriendUpdate(Resource):
         cache.clear()
         return friend_schema.dump(friend)
 
+    @api.doc(responses={200: 'Success', 401: 'Unauthorized', 400:'Bad Request'})
     def delete(self, friend_id):
         try:
             session["user_id"]
--- application/users/views.py
+++ application/users/views.py
@@ -1,19 +1,42 @@
-from flask_restx import Resource
+from flask_restx import Resource, fields
 from .models import Users, UsersSchema
 from application import db, cache
 from flask import request, session, abort
 from marshmallow import ValidationError
 from sqlalchemy.exc import IntegrityError
+from . import api
 
 user_schema = UsersSchema()
 users_schema = UsersSchema(many=True)
 
+user_fields = api.model('User',{
+    "id" : fields.Integer(required=True),
+    "email": fields.String(required=True),
+    "first_name": fields.String(required=True),
+    "last_name": fields.String(required=True)
+})
+
+user_create_fields = api.model('UserCreate',{
+    "email": fields.String(required=True),
+    "password": fields.String(required=True),
+    "first_name": fields.String(required=True),
+    "last_name": fields.String(required=True)
+})
+
+user_logIn_fields = api.model('UserLogIn',{
+    "email": fields.String(required=True),
+    "password": fields.String(required=True),
+})
+
 class UsersList(Resource):
 
+    @api.doc(responses={400: 'Bad Request'})
+    @api.marshal_with(user_fields, as_list=True)
     def get(self):
         users=Users.query.all()
         return users_schema.dump(users)
 
+    @api.doc(responses={200: ('Success', user_fields), 400:'Bad Request', 401: 'Unauthorized'}, body=user_create_fields)
     def post(self):
         try:
             user_schema.load(request.json)
@@ -43,6 +66,7 @@ class UsersList(Resource):
 
 class AuthUser(Resource):
 
+    @api.doc(responses={200: ('Success', user_fields), 400: 'Bad Request'},body=user_logIn_fields)
     def post(self):
         email = request.json["email"]
         password = request.json["password"]
@@ -60,6 +84,7 @@ class AuthUser(Resource):
         session['user_id']=user.id
         return user_schema.dump(user)
 
+    @api.doc(responses={200: ('Success', user_fields), 400: 'Bad Request'})
     def delete(self):
         try:
             session.pop("user_id")
