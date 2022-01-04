3536852615405702460131591fb0cc1e9397b5ea
Mihai405 <mihaijurj2000@yahoo.com>
2022-01-04 18:02:39 +0200

    Allow Cors

--- application/__init__.py
+++ application/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_caching import Cache
+from flask_cors import CORS
 import os
 
 db = SQLAlchemy()
@@ -11,6 +12,7 @@ cache = Cache(config={'CACHE_TYPE': 'SimpleCache'})
 def create_app(test_config=None):
     basedir = os.path.abspath(os.path.dirname(__file__))
     app = Flask(__name__)
+    CORS(app)
     app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
